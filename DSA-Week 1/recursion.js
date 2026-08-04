fad7a8628179c398568348ee0ea171dafc622b06
Abhishek <abhishekpalayadan20@gmail.com>
2026-08-04 20:00:02 +0530

    recursion updated 2

--- DSA-Week 1/recursion.js
+++ DSA-Week 1/recursion.js
@@ -17,3 +17,39 @@ function printReverse(n){
     console.log(n);
     printReverse(n-1);
 }
+
+//sum of first n numbers
+
+function sumOfNNumbers(n){
+    if(n<1){
+        return 0;
+    }
+    return n+sumOfNNumbers(n-1);
+}
+
+//Factorial of numbers
+
+function factorial(n){
+    if(n<=1){
+        return 1;
+    }
+    return n*(factorial(n-1));
+}
+
+//Power of number
+
+function power(base,exponent){
+    if(exponent<=0){
+        return 1;
+    }
+    return base*power(base,exponent-1);
+}
+
+//Reverse string
+
+function reverseString(str){
+    if(str.length===0){
+        return "";
+    }
+    return reverseString(str.slice(1))+str[0];
+}
