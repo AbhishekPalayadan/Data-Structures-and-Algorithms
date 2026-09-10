388705a04d92c8c23672241986f45ad31d83f7c4
Abhishek <abhishekpalayadan20@gmail.com>
2026-09-10 20:23:38 +0530

    insertion sort added

---
+++ DSA-Week 2/Sorting/InsertionSort.js
@@ -0,0 +1,15 @@
+function insertionSort(arr){
+    for(let i=1;i<arr.length;i++){
+        let current=arr[i];
+        let j=i-1;
+
+        while(j>=0 && arr[j]>current){
+            arr[j+1]=arr[j];
+            j--;
+        }
+        arr[j+1]=current;
+    }
+    return arr;
+}
+const arr=[6,0,9,4,7,2,8,3,5,1]
+console.log(insertionSort(arr))
