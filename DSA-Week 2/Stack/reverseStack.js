ec175cc844893a810f7a2689e39d8ec51f0f8cfa
Abhishek <abhishekpalayadan20@gmail.com>
2026-09-12 02:37:13 +0530

    reverse Stack added

---
+++ DSA-Week 2/Stack/reverseStack.js
@@ -0,0 +1,10 @@
+function reverseStack(stack){
+    let tempStack=new Stack;
+    while(!stack.isEmpty()){
+        tempStack.push(stack.pop())
+    }
+    return tempStack;
+}
+
+const reversed=reverseStack(stack);
+reversed.display()
